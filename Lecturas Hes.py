111e949457f5048abbedf428cb267aa4f3a052ee
Miaa <pedroguillermomiaa@gmail.com>
2026-03-12 16:35:03 -0600

    Update Lecturas Hes.py

--- Lecturas Hes.py
+++ Lecturas Hes.py
@@ -19,7 +19,7 @@ st.markdown("""
         /* 1. POSICIONAR EL TÍTULO EN LA BARRA SUPERIOR */
         .titulo-superior {
             position: fixed;
-            top: 5;
+            top: 2;
             left: 50%;
             transform: translateX(-50%);
             z-index: 999999;
@@ -287,6 +287,7 @@ with col_der:
 
 if st.button("🔄 Reiniciar Tablero", use_container_width=True):
     reiniciar_tablero()
+
 
 
 
