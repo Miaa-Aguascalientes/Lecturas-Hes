6c5be7b9334456230710000a0abee1ce19ee6a1e
Miaa <pedroguillermomiaa@gmail.com>
2026-03-13 10:17:23 -0600

    Update Lecturas Hes.py

--- Lecturas Hes.py
+++ Lecturas Hes.py
@@ -44,13 +44,13 @@ st.markdown("""
         /* AJUSTE PARA HACER MÁS CHICO EL LOGO */
         [data-testid="stSidebarUserContent"] img {
             margin-top: -70px !important; 
-            max-width: 150px !important; /* Ajusta este valor (ej. 100px, 120px) para reducir el tamaño */
+            max-width: 200px !important; /* Ajusta este valor (ej. 100px, 120px) para reducir el tamaño */
             margin-left: auto;
             margin-right: auto;
             display: block;
         }
         [data-testid="stSidebarUserContent"] img {
-            margin-top: -70px !important; 
+            margin-top: -80px !important; /* Ajusta este valor para subirlo más o menos */
         }
 
         /* 3. ELIMINAR EL ESPACIO SUPERIOR DEL CONTENEDOR PRINCIPAL */
@@ -336,6 +336,7 @@ with col_der:
 
 if st.button("🔄 Reiniciar Tablero", use_container_width=True):
     reiniciar_tablero()
+
 
 
 
