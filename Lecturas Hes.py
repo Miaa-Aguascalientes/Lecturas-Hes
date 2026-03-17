ab8ee333b05162c4114383d2911007a699c30b59
Miaa <pedroguillermomiaa@gmail.com>
2026-03-17 11:56:29 -0600

    Update Lecturas Hes.py

--- Lecturas Hes.py
+++ Lecturas Hes.py
@@ -13,7 +13,7 @@ import time
 # 1. CONFIGURACIÓN
 st.set_page_config(
     page_title="MIAA - Tablero de Consumos",
-    page_icon="https://raw.githubusercontent.com/Miaa-Aguascalientes/Lecturas-Hes/refs/heads/main/favicon.ico", 
+    page_icon="https://www.miaa.mx/favicon.ico", 
     layout="wide"  
 )
 
